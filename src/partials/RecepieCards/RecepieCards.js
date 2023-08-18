06776594b1fa7b8659595f98b98eb3a272a7fa68
Orokana seikō <94075384+app1e1over@users.noreply.github.com>
2023-08-18 10:19:08 +0300

    added scripts to favorites + work on recepie cards

--- src/partials/RecepieCards/RecepieCards.js
+++ src/partials/RecepieCards/RecepieCards.js
@@ -1,6 +1,46 @@
-export function Update(){
+import axios from "axios";
 
+export function Update(){
+    let req = MakeRequestString();
+    console.log(req);
+    let cont = document.querySelector(".recepie-container");
+    axios.get(req).then(v=>v.data.results).then(recepies=>{
+        for(let recepy of recepies){
+            console.log();
+            let wrap = document.createElement("div");
+            wrap.innerHTML = recepy.title;
+            cont.append(wrap);
+        }
+    })
 }
+Update();
 function MakeRequestString(){
-    
+    const selectIngredients = document.getElementById('selectIng');
+    const selectCountry = document.getElementById('selectCountry');
+    const selectTime = document.getElementById('selectTime');
+    const search = document.querySelector('.search');
+    const category = document.getElementById('categories');
+    const paginator = document.getElementById('pagination');
+    let obj = {
+        category: category,
+        page: paginator,
+        time: selectTime,
+        area:selectCountry,
+        ingredients: selectIngredients
+    }
+    let first = true;
+
+    let str="https://tasty-treats-backend.p.goit.global/api/recipes";
+    for(let key in obj){
+        if(obj[key]!=undefined && obj[key].value!=undefined){
+            if(!first){
+                str+="&"
+            }else{
+                str+="?";
+                first=false;
+            }
+            str+=key+"="+obj[key].value;
+        }
+    }
+    return str;
 }
