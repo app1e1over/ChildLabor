490b86e686c0e621f8219a369e0af4e1d9feeab3
Orokana seikō <94075384+app1e1over@users.noreply.github.com>
2023-08-19 10:46:30 +0300

    added rating and button to cards

--- src/partials/RecepieCards/RecepieCards.js
+++ src/partials/RecepieCards/RecepieCards.js
@@ -1,41 +1,86 @@
 import axios from 'axios';
-
+function padEndRating(subj) {
+  subj = subj.toString();
+  if( subj.length===1){
+    return subj+'.0';
+  }else{
+    if(subj.length>3){
+      return subj.slice(0, 3);
+    }
+  }
+  return subj;
+}
 export function Update() {
   let req = MakeRequestString();
   console.log(req);
-  let cont = document.querySelector('.recepy-container');
+  let cont = document.querySelector('.recipe-container');
   axios
     .get(req)
     .then(v => v.data.results)
     .then(recepies => {
-      for (let recepy of recepies) {
-        cont.append(DrawCard(recepy));
+      for (let recipe of recepies) {
+        cont.append(DrawCard(recipe));
       }
     });
 }
-function DrawCard(recepy) {
+function DrawCard(recipe) {
   let cont = document.createElement('div');
-  cont.classList.add('recepy-card-container');
+  cont.classList.add('recipe-card-container');
 
   //creating background image
   let bg = document.createElement('img');
-  bg.src = recepy.thumb;
-  bg.classList.add('recepy-card-bg');
+  bg.src = recipe.thumb;
+  bg.classList.add('recipe-card-bg');
+
+  let wrap = document.createElement("div");
+  wrap.classList.add("recipe-card-wrap");
 
   let textContainer = document.createElement("div");
-  textContainer.classList.add("recepy-card-text")
+  textContainer.classList.add("recipe-card-text")
   let name = document.createElement('p');
-  name.classList.add('recepy-card-title');
-  name.innerText = recepy.title;
+  name.classList.add('recipe-card-title');
+  name.innerText = recipe.title;
 
   let description = document.createElement('p');
-  description.classList.add('recepy-card-description');
-description.innerText=recepy.description;
+  description.classList.add('recipe-card-description');
+description.innerText=recipe.description;
+
+  let like = document.createElement("button");
+  like.classList.add("heart-button");
+  like.classList.add("recipe-card-like-btn");
+  like.innerText="like";
+
+
+  let lowerContainer = document.createElement("div");
+  lowerContainer.classList.add("recipe-card-lower-container");
+
+
+  let rating = document.createElement("div");
+  rating.classList.add("recipe-card-rating");
+  rating.innerText=padEndRating(recipe.rating);
+  for(let i=1;i<=5; i++){
+    let svg = document.createElement("img");
+    svg.setAttribute('width', '18');
+    svg.setAttribute('height', '18');
+    svg.src = "../../image/sprite.svg"
+    rating.appendChild(svg);
+  }
+  lowerContainer.appendChild(rating);
+
+  let seeRecep = document.createElement("button");
+  seeRecep.classList.add("recipe-card-see-recipe")
+seeRecep.innerText = "See recipe"
+lowerContainer.appendChild(seeRecep);
 
   cont.appendChild(bg);
   textContainer.appendChild(name);
   textContainer.appendChild(description);
-  cont.appendChild(textContainer);
+  cont.appendChild(like);
+
+  wrap.appendChild(textContainer);
+  wrap.appendChild(lowerContainer);
+
+  cont.appendChild(wrap);
   return cont;
 }
 function MakeRequestString() {
