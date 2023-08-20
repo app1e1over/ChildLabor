93f4d82dea8386f5f3c87987a7b2385bee9ec194
Mia4678 <solomia.lutska@gmail.com>
2023-08-20 20:40:23 +0200

    new update slider dont work

--- src/partials/HeroCard/HeroCard.js
+++ src/partials/HeroCard/HeroCard.js
@@ -2,174 +2,274 @@
 
 
 // export async function getMasterclass() {
-//   try {
-//     const response = await axios.get('https://tasty-treats-backend.p.goit.global/api/events');
-//     const events = response.data;
+    //   try {
+    //     const response = await axios.get('https://tasty-treats-backend.p.goit.global/api/events');
+    //     const events = response.data;
+        
+    //     const slider = document.querySelector('.slider-container'); 
     
-//     const slider = document.querySelector('.slider-container'); 
-
-//     events.forEach(event => {
-//       const card = document.createElement('div');
-//       card.classList.add('event-card');
-      
-//       const chefImage = new Image();
-//       chefImage.src = event.cook.imgUrl;
-//       card.appendChild(chefImage);
-      
-//       const eventName = document.createElement('h2');
-//       eventName.textContent = event.topic.name;
-//       card.appendChild(eventName);
-      
-//       const dishRegion = document.createElement('p');
-//       dishRegion.textContent = event.topic.area;
-//       card.appendChild(dishRegion);
-      
-//       const previewImage = new Image();
-//       previewImage.src = event.topic.previewUrl;
-//       card.appendChild(previewImage);
-      
-//       const dishImage = new Image();
-//       dishImage.src = event.topic.imgUrl;
-//       card.appendChild(dishImage);
-      
-//       slider.appendChild(card);
-//     });
+    //     events.forEach(event => {
+    //       const card = document.createElement('div');
+    //       card.classList.add('event-card');
+          
+    //       const chefImage = new Image();
+    //       chefImage.src = event.cook.imgUrl;
+    //       card.appendChild(chefImage);
+          
+    //       const eventName = document.createElement('h2');
+    //       eventName.textContent = event.topic.name;
+    //       card.appendChild(eventName);
+          
+    //       const dishRegion = document.createElement('p');
+    //       dishRegion.textContent = event.topic.area;
+    //       card.appendChild(dishRegion);
+          
+    //       const previewImage = new Image();
+    //       previewImage.src = event.topic.previewUrl;
+    //       card.appendChild(previewImage);
+          
+    //       const dishImage = new Image();
+    //       dishImage.src = event.topic.imgUrl;
+    //       card.appendChild(dishImage);
+          
+    //       slider.appendChild(card);
+    //     });
+        
+    //     return response;
+    //   } catch (error) {
+    //     console.error('An error occurred:', error);
+    //   }
+    // }
+    // getMasterclass();
     
-//     return response;
-//   } catch (error) {
-//     console.error('An error occurred:', error);
-//   }
-// }
-// getMasterclass();
-
-// export function getMasterclass() {
-//   const slider = document.querySelector('.slider-container'); 
-//   axios.get('https://tasty-treats-backend.p.goit.global/api/events')
-//     .then(response => {
-//       const events = response.data;
-
-//       events.forEach(event => {
-//         const card = DrawEventCard(event);
-//         slider.appendChild(card);
-//       });
-//     })
-//     .catch(error => {
-//       console.error('An error occurred:', error);
-//     });
-// }
-
-import axios from "axios";
-
-export function eventUpdate() {
-  let req = MakeRequestString();
-  console.log(req);
-  let cont = document.querySelector('.slider-container');
-  axios
-    .get(req)
-    .then(response => {
-      console.log(response.data); 
-      return response.data.results;
-  })
-    .then(events => {
-      for (let event of events) {
-        cont.append(DrawEventCard(event));
+    // export function getMasterclass() {
+    //   const slider = document.querySelector('.slider-container'); 
+    //   axios.get('https://tasty-treats-backend.p.goit.global/api/events')
+    //     .then(response => {
+    //       const events = response.data;
+    
+    //       events.forEach(event => {
+    //         const card = DrawEventCard(event);
+    //         slider.appendChild(card);
+    //       });
+    //     })
+    //     .catch(error => {
+    //       console.error('An error occurred:', error);
+    //     });
+    // }
+    
+    
+    import axios from "axios";
+    import Swiper from 'swiper';
+    import 'swiper/swiper-bundle.css';
+    
+    export function getMasterClasses() {
+      let req = MakeRequestString();
+      console.log(req);
+      let cont = document.querySelector('.swiper-wrapper');
+      axios
+        .get(req)
+        .then(response => {
+          console.log(response.data); 
+          return response.data;
+      })
+        .then(events => {
+          for (let event of events) {
+            cont.append(DrawEventCard(event));
+          } 
+        })
+        .catch(error => {
+          console.error("Помилка запиту до сервера:", error);
+        })
+        .finally( () =>  {
+          new Swiper('.swiper-container', {
+            slidesPerView: 'auto', // Встановлюємо кількість слайдів, які видно одночасно
+            spaceBetween: 16, // Відступ між слайдами
+            pagination: {
+              el: '.swiper-pagination', // Додаємо пагінацію (dots)
+              clickable: true, // Зробити dots клікабельними
+            },
+            // Додайте інші параметри Swiper за потреби
+          });
+        })
       }
-    })
-    .catch(error => {
-      console.error("Помилка запиту до сервера:", error);
-    });
-  }
-function DrawEventCard(event) {
-  const card = document.createElement('div');
-  card.classList.add('event-card');
-
-  const chefImage = createImage(event.cook.imgUrl);
-  card.appendChild(chefImage);
- 
-  const eventName = createHeading('h2', event.topic.name);
-  card.appendChild(eventName);
-
-  const dishRegion = createParagraph(event.topic.area);
-  card.appendChild(dishRegion);
-
-  const previewImage = createImage(event.topic.previewUrl);
-  card.appendChild(previewImage);
-
-  const dishImage = createImage(event.topic.imgUrl);
-  card.appendChild(dishImage);
-
-  return card;
-}
-
-eventUpdate();
-
-// function MakeRequestString() {
-//   const selectIngredients = document.getElementById('selectIng');
-//   const selectCountry = document.getElementById('selectCountry');
-//   const selectTime = document.getElementById('selectTime');
-//   const search = document.querySelector('.search');
-//   const category = document.getElementById('categories');
-//   const paginator = document.getElementById('pagination');
-//   let obj = {
-//     category: category,
-//     page: paginator,
-//     time: selectTime,
-//     area: selectCountry,
-//     ingredients: selectIngredients,
-//   };
-//   let first = true;
-
-//   let str = 'https://tasty-treats-backend.p.goit.global/api/recipes';
-//   for (let key in obj) {
-//     if (obj[key] != undefined && obj[key].value != undefined) {
-//       if (!first) {
-//         str += '&';
-//       } else {
-//         str += '?';
-//         first = false;
-//       }
-//       str += key + '=' + obj[key].value;
-//     }
-//   }
-//   return str;
-// }
-
-function MakeRequestString() {
-  const baseUrl = "https://tasty-treats-backend.p.goit.global"; 
-
-  const resource = "/api/events"; 
-
-  const requestUrl = `${baseUrl}${resource}`;
-
-  return requestUrl;
-}
-
-function createImage(src) {
-  const image = document.createElement('img');
-  image.src = src;
-  return image;
-}
-
-function createHeading(headingType, text) {
-  const heading = document.createElement(headingType);
-  heading.textContent = text;
-  return heading;
-}
-
-function createParagraph(text) {
-  const paragraph = document.createElement('p');
-  paragraph.textContent = text;
-  return paragraph;
-}
-
-
-
-// const masterclassDescription = document.querySelector(".masterclass-preview");
-// const title = "Learn to Cook Tasty Treats'Customizable Masterclass";
-// const description = "TastyTreats - Customize Your Meal with Ingredient Options and Step-by-Step Video Guides.";
-
-
-// const template = `  <h2 class="title">${title}</h2>
-//                     <p class="description">${description}</p>`;
-
-// masterclassDescription.innerHTML = template;
+      
+    function DrawEventCard(event) {
+      const card = document.createElement('div');
+      card.classList.add('event-card');
+    
+      const chefImage = createImage(event.cook.imgUrl);
+      chefImage.setAttribute("width", 150)
+      card.appendChild(chefImage);
+     
+      const eventName = createHeading('h2', event.topic.name);
+      card.appendChild(eventName);
+    
+      const dishRegion = createParagraph(event.topic.area);
+      card.appendChild(dishRegion);
+    
+      const previewImage = createImage(event.topic.previewUrl);
+      card.appendChild(previewImage);
+    
+      const dishImage = createImage(event.topic.imgUrl);
+      card.appendChild(dishImage);
+    
+      return card;
+    }
+    
+    getMasterClasses();
+    
+    // const sliderWrapperElement = document.querySelector('.swiper-wrapper');
+    // const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/events';
+    // console.log(BASE_URL);
+    
+    // async function fetchMasterClasses() {
+    //   try {
+    //     const response = await axios.get(BASE_URL);
+    //     return response.data;
+    //   } catch (error) {
+    //     console.error(error);
+    //     return [];
+    //   }
+    // }
+    
+    // fetchMasterClasses();
+    
+    // const item = [];
+    
+    // function createSlideMarkup(item) {
+    //   const { cook, topic } = item;
+    //   return 
+    //     `<div class="swiper-slide">
+    //       <img src="${cook.imgUrl}" alt="${cook.name}" class="cook-img" style="width: 182px; height: 442px; margin-right: 16px; border-radius: 15px">
+    //       </div>
+    //        <div class="swiper-slide">
+    //       <img src="${topic.previewUrl}" alt="${topic.name}" class="dish-img middle-img">
+    //       <div class="content">
+    //         <h2 class="slider-title">${topic.name}</h2>
+    //         <p class="slider-text">${topic.area}</p>
+    //       </div>
+    //       </div>
+    //        <div class="swiper-slide">
+    //       <img src="${topic.imgUrl}" alt="${topic.name}" class="dish-img">
+    //       </div>
+    //     </div>`   ;
+    // }
+    
+    // const addGalleryMarkup = createSlideMarkup(item);
+    
+    // sliderWrapperElement.innerHTML = addGalleryMarkup;
+    
+    function MakeRequestString() {
+      const baseUrl = "https://tasty-treats-backend.p.goit.global"; 
+    
+      const resource = "/api/events"; 
+    
+      const requestUrl = `${baseUrl}${resource}`;
+    
+      return requestUrl;
+    }
+    
+    function createImage(src) {
+      const image = document.createElement('img');
+      image.src = src;
+      return image;
+    }
+    
+    function createHeading(headingType, text) {
+      const heading = document.createElement(headingType);
+      heading.textContent = text;
+      return heading;
+    }
+    
+    function createParagraph(text) {
+      const paragraph = document.createElement('p');
+      paragraph.textContent = text;
+      return paragraph;
+    }
+    
+    
+    // import axios from "axios";
+    // import Swiper from 'swiper/swiper-bundle.min'; // Імпорт Swiper з npm
+    
+    
+    // // Отримайте елементи DOM
+    // const sliderContainer = document.querySelector('.swiper-container');
+    // const sliderWrapper = sliderContainer.querySelector('.swiper-wrapper');
+    
+    // // Отримайте дані з бекенду, які містять фотографії
+    // function fetchMasterClasses() {
+    //   const req = MakeRequestString();
+    //   axios
+    //     .get(req)
+    //     .then(response => {
+    //       const events = response.data;
+    //       for (const event of events) {
+    //         // Створюємо елементи для фотографій кухарів і додаємо їх до слайдера
+    //         const card = DrawEventCard(event);
+    //         sliderWrapper.appendChild(card);
+    //       }
+    
+    //       // Після додавання всіх слайдів налаштуйте Swiper
+    //       new Swiper(sliderContainer, {
+    //         slidesPerView: 'auto', // Встановлюємо кількість слайдів, які видно одночасно
+    //         spaceBetween: 16, // Відступ між слайдами
+    //         pagination: {
+    //           el: '.swiper-pagination', // Додаємо пагінацію (dots)
+    //           clickable: true, // Зробити dots клікабельними
+    //         },
+    //         // Додайте інші параметри Swiper за потреби
+    //       });
+    //     })
+    //     .catch(error => {
+    //       console.error("Помилка запиту до сервера:", error);
+    //     });
+    // }
+    
+    // // Викликати функцію для отримання даних та ініціалізації слайдера
+    // fetchMasterClasses();
+    
+    // function MakeRequestString() {
+    //   const baseUrl = "https://tasty-treats-backend.p.goit.global"; 
+    //   const resource = "/api/events"; 
+    //   const requestUrl = `${baseUrl}${resource}`;
+    //   return requestUrl;
+    // }
+    
+    // function createImage(src) {
+    //   const image = document.createElement('img');
+    //   image.src = src;
+    //   return image;
+    // }
+    
+    // function createHeading(headingType, text) {
+    //   const heading = document.createElement(headingType);
+    //   heading.textContent = text;
+    //   return heading;
+    // }
+    
+    // function createParagraph(text) {
+    //   const paragraph = document.createElement('p');
+    //   paragraph.textContent = text;
+    //   return paragraph;
+    // }
+    //   function DrawEventCard(event) {
+    //     const card = document.createElement('div');
+    //     card.classList.add('swiper-slide'); // Додали клас для слайду
+    //       const chefImage = createImage(event.cook.imgUrl);
+    //       card.appendChild(chefImage);
+         
+    //       const eventName = createHeading('h2', event.topic.name);
+    //       card.appendChild(eventName);
+        
+    //       const dishRegion = createParagraph(event.topic.area);
+    //       card.appendChild(dishRegion);
+        
+    //       const previewImage = createImage(event.topic.previewUrl);
+    //       card.appendChild(previewImage);
+        
+    //       const dishImage = createImage(event.topic.imgUrl);
+    //       card.appendChild(dishImage);
+        
+    //       return card;
+    //     }
