988896a859161441a9c746aef538860d8d990974
Orokana seikō <94075384+app1e1over@users.noreply.github.com>
2023-08-21 14:29:52 +0300

    fixed issues with favorites page

---
+++ src/favorites.js
@@ -0,0 +1,6 @@
+import {UpdateFavorites} from "./partials/Favorites/Favorites.js";
+import { startHeader } from "./partials/Header/Header.js";
+import { StartScrollUp } from "./partials/ScrollUp/ScrollUp.js";
+
+StartScrollUp();
+startHeader();
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import {UpdateFavorites} from "./partials/Favorites/Favorites.js";
 import { Update } from "./partials/RecepieCards/RecepieCards.js";
 import { startHeader } from "./partials/Header/Header.js";
 import { getMasterClasses } from "./partials/HeroCard/HeroCard.js";
