ef828297f06d9539c22087d8ce0c2e60aaee3839
Matsiiovska <125771191+Matsiiovska@users.noreply.github.com>
2023-08-21 12:21:42 +0300

    scroll i function

--- src/partials/Categories/Categories.js
+++ src/partials/Categories/Categories.js
@@ -36,32 +36,3 @@ categoriesContainer.value = ""; // присвоюємо пусту строку 
 
 
  
-const scrollToTopBtn = document.getElementById("scrollToTopBtn");
-const header = document.querySelector("header");
-
-// Визначення видимості хедера
-function isHeaderVisible() {
-  let headerRect = header.getBoundingClientRect();
-  let headerBottom = headerRect.bottom || headerRect.y + headerRect.height;
-  return headerBottom > 0;
-}
-
-// Показати або приховати кнопку при прокрутці
-window.addEventListener("scroll", function() {
-  if (isHeaderVisible()) {
-    scrollToTopBtn.classList.remove("show");
-  } else {
-    scrollToTopBtn.classList.add("show");
-  }
-});
-
-// Прокрутка до верху при натисканні кнопки
-scrollToTopBtn.addEventListener("click", function() {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
-});     
-
-
-
