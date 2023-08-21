9ce09668a555a77eeeacbe1a1a99ff6b8e565a41
Orokana seikō <94075384+app1e1over@users.noreply.github.com>
2023-08-21 10:02:34 +0300

    added scroll up

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import axios from "axios";
 import {UpdateFavorites} from "./partials/Favorites/Favorites.js";
 import { Update } from "./partials/RecepieCards/RecepieCards.js";
 import { startHeader } from "./partials/Header/Header.js";
-
+import { getMasterClasses } from "./partials/HeroCard/HeroCard.js";
 
 Update();
 startHeader();
 UpdateFavorites();
+getMasterClasses();
