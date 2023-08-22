6fea57d55d01c430535740dd97749851bad4f5b1
kaonashiiiiiii <priora102003@gmail.com>
2023-08-22 11:53:01 +0300

    some fixes

--- src/partials/ModalWindow/ModalWindow.js
+++ src/partials/ModalWindow/ModalWindow.js
@@ -1,15 +1,15 @@
 export const ModalStart = () => {
-  const openModalBtns = document.querySelectorAll("[data-modal-open]"); // Знайдіть всі кнопки, які відкривають модальне вікно
+  const openModalBtns = document.querySelectorAll("[data-modal-open]");
   const closeModalBtn = document.querySelector("[data-modal-close]");
   const modal = document.querySelector("[data-modal]");
   const backdrop = document.querySelector(".backdrop");
-  // Додайте обробники подій для кожної кнопки, яка відкриває модальне вікно
   openModalBtns.forEach(btn => {
+      console.log(btn); // Вивести в консоль поточну кнопку
       btn.addEventListener("click", toggleModal);
   });
   closeModalBtn.addEventListener("click", toggleModal);
   backdrop.addEventListener("click", toggleModal);
-  function toggleModal() {
+  function toggleModal(e) {
       modal.classList.toggle("is-hidden");
       backdrop.classList.toggle("is-hidden");
       document.body.classList.toggle("modal-open");
