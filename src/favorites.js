6306e2a5fe07ae5fb6355924e72872475939729b
Orokana seikō <94075384+app1e1over@users.noreply.github.com>
2023-08-24 10:22:40 +0300

    фввув

--- src/favorites.js
+++ src/favorites.js
@@ -1,7 +1,9 @@
 import {StartFavorites} from "./partials/Favorites/Favorites.js";
 import { startHeader } from "./partials/Header/Header.js";
 import { StartScrollUp } from "./partials/ScrollUp/ScrollUp.js";
+import { updateHearts } from "./partials/FavoritesFilter/FavoritesFilter.js";
 
 StartScrollUp();
 startHeader();
 StartFavorites();
+updateHearts();
