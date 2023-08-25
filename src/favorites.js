d3c721328b7d1db78457b709baf0fe428242aa8f
Orokana seikō <94075384+app1e1over@users.noreply.github.com>
2023-08-25 14:15:17 +0300

    center tablet

--- src/favorites.js
+++ src/favorites.js
@@ -2,12 +2,13 @@ import { StartFavorites } from './partials/Favorites/Favorites.js';
 import { startHeader } from './partials/Header/Header.js';
 import { StartScrollUp } from './partials/ScrollUp/ScrollUp.js';
 import { updateHearts } from './partials/FavoritesFilter/FavoritesFilter.js';
+import { ModalStart } from './partials/ModalWindow/ModalWindow.js';
 //import 'normalize.css';
 
 
 
 StartScrollUp();
-
+ModalStart();
 try {
   startHeader();
 } catch (er) {
